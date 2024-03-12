780214b43e06ad3d27710effeee62379a3e40d80
Giovanna <torresssgio@gmail.com>
2024-03-12 12:38:39 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program 
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu nome");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Seu nome tem {nome.Length} caracteres");
+
+            Console.WriteLine("Digite a data do seu nascimento:");
+            DateTime dtNascimento = DateTime.Parse(Console.ReadLine());
+
+            int qtsDiasVividos = DateTime.Now.Subtract(dtNascimento).Days;
+            Console.WriteLine("Os dias vividos até hoje são: " + qtsDiasVividos);
+
+            Console.ReadKey();
+            ;
+        }
+    }
+}
+
+
